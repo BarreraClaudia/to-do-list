92edda1a7e99359fc3ad0047a88420ca5c84a1bb
BarreraClaudia <claudiabarrera.code@gmail.com>
2025-01-28 13:59:02 -0700

    added login and logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
-const { signupSchema } = require('../middlewares/validator');
+const { signupSchema, loginSchema } = require('../middlewares/validator');
 const User = require('../models/User');
-const { doHash } = require('../utils/hashing');
+const { doHash, doHashValidation } = require('../utils/hashing');
+const jwt = require('jsonwebtoken');
 
 exports.getSignup = (req, res) => {
   if (req.user) {
@@ -41,12 +42,70 @@ exports.postSignup = async (req, res) => {
 
     result.password = undefined;
 
-    res.status(201).json({
-      success: true,
-      message: 'Your account has been created successfully',
-      result,
-    });
+    res.redirect('/todo');
   } catch (error) {
     console.log(error);
   }
+};
+
+exports.getLogin = async (req, res) => {
+  if (req.user) {
+    return res.redirect('/todo');
+  }
+  res.render('login');
+};
+
+exports.postLogin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const { error, value } = loginSchema.validate({ email, password });
+
+    if (error) {
+      return res
+        .status(401)
+        .json({ success: false, message: error.details[0].message });
+    }
+
+    const existingUser = await User.findOne({ email }).select('+password');
+
+    if (!existingUser) {
+      return res
+        .status(401)
+        .json({ success: false, message: 'User does not exist!' });
+    }
+
+    const result = await doHashValidation(password, existingUser.password);
+
+    if (!result) {
+      return res
+        .status(401)
+        .json({ success: false, message: 'Invalid password!' });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: existingUser._id,
+        email: existingUser.email,
+        verified: existingUser.verified,
+      },
+      process.env.TOKEN_SECRET,
+      {
+        expiresIn: '8h',
+      }
+    );
+
+    res
+      .cookie('Authorization', 'Bearer' + token, {
+        expires: new Date(Date.now() + 8 * 3600000),
+        httpOnly: process.env.NODE_ENV === 'production',
+        secure: process.env.NODE_ENV === 'production',
+      })
+      .redirect('/todo');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.logout = async (req, res) => {
+  res.clearCookie('Authorization').status(200).redirect('/');
 };
--- routes/main.js
+++ routes/main.js
@@ -6,5 +6,8 @@ const authController = require('../controllers/auth');
 router.get('/', homeController.getIndex);
 router.get('/signup', authController.getSignup);
 router.post('/signup', authController.postSignup);
+router.get('/login', authController.getLogin);
+router.post('/login', authController.postLogin);
+router.get('/logout', authController.logout);
 
 module.exports = router;
