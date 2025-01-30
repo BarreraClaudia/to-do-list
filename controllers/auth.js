aacd20037a8fcf83e9b4b3c0630ec405fcac3252
BarreraClaudia <claudiabarrera.code@gmail.com>
2025-01-30 14:48:23 -0700

    added change password, forgot password, and identification middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,13 @@
+const jwt = require('jsonwebtoken');
 const {
   signupSchema,
   loginSchema,
   acceptCodeSchema,
+  changePasswordSchema,
+  acceptFPCodeSchema,
 } = require('../middlewares/validator');
 const User = require('../models/User');
 const { doHash, doHashValidation, hmacProcess } = require('../utils/hashing');
-const jwt = require('jsonwebtoken');
 const transport = require('../middlewares/sendMail');
 
 exports.getSignup = (req, res) => {
@@ -100,12 +102,16 @@ exports.postLogin = async (req, res) => {
     );
 
     res
-      .cookie('Authorization', 'Bearer' + token, {
+      .cookie('Authorization', 'Bearer ' + token, {
         expires: new Date(Date.now() + 8 * 3600000),
         httpOnly: process.env.NODE_ENV === 'production',
         secure: process.env.NODE_ENV === 'production',
       })
-      .redirect('/todo');
+      .json({
+        success: true,
+        token,
+        message: 'logged in successfully',
+      });
   } catch (error) {
     console.log(error);
   }
@@ -138,7 +144,7 @@ exports.sendVerificationCode = async (req, res) => {
     let info = await transport.sendMail({
       from: process.env.NODE_CODE_SENDING_EMAIL_ADDRESS,
       to: existingUser.email,
-      subject: 'verification code',
+      subject: 'Verification code',
       html: '<h1>' + codeValue + '</h1>',
     });
 
@@ -172,7 +178,7 @@ exports.verifyVerificationCode = async (req, res) => {
 
     const codeValue = providedCode.toString();
     const existingUser = await User.findOne({ email }).select(
-      '+verificationCode + verificationCodeValidation'
+      '+verificationCode +verificationCodeValidation'
     );
 
     if (!existingUser) {
@@ -216,6 +222,162 @@ exports.verifyVerificationCode = async (req, res) => {
       return res
         .status(200)
         .json({ success: true, message: 'Your account has been verified!' });
+    }
+    return res
+      .status(400)
+      .json({ success: false, message: 'unexpected occurred!' });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.changePassword = async (req, res) => {
+  const { userId, verified } = req.user;
+  const { oldPassword, newPassword } = req.body;
+  try {
+    const { error, value } = changePasswordSchema.validate({
+      oldPassword,
+      newPassword,
+    });
+
+    if (error) {
+      return res
+        .status(401)
+        .json({ success: false, message: error.details[0].message });
+    }
+
+    if (!verified) {
+      return res
+        .status(401)
+        .json({ success: false, message: 'You are not a verified user!' });
+    }
+
+    const existingUser = await User.findOne({ _id: userId }).select(
+      '+password'
+    );
+
+    if (!existingUser) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'User does not exist!' });
+    }
+
+    const result = await doHashValidation(oldPassword, existingUser.password);
+
+    if (!result) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Invalid password!' });
+    }
+
+    const hashedPassword = await doHash(newPassword, 12);
+    existingUser.password = hashedPassword;
+    await existingUser.save();
+    return res
+      .status(200)
+      .json({ success: true, message: 'Password updated!' });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.sendForgotPasswordCode = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'User does not exist!' });
+    }
+
+    //can also use node js crypto module to generate code
+    const codeValue = Math.floor(Math.random() * 1000000).toString();
+
+    let info = await transport.sendMail({
+      from: process.env.NODE_CODE_SENDING_EMAIL_ADDRESS,
+      to: existingUser.email,
+      subject: 'Forgot password code',
+      html: '<h1>' + codeValue + '</h1>',
+    });
+
+    if (info.accepted[0] === existingUser.email) {
+      const hashedCodeValue = hmacProcess(
+        codeValue,
+        process.env.HMAC_VERIFICATION_CODE_SECRET
+      );
+      existingUser.forgotPasswordCode = hashedCodeValue;
+      existingUser.forgotPasswordCodeValidation = Date.now();
+      await existingUser.save();
+      return res.status(200).json({ success: true, message: 'Code sent!' });
+    }
+    res.status(400).json({ success: false, message: 'Code failed to send!' });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.verifyForgotPasswordCode = async (req, res) => {
+  const { email, providedCode, newPassword } = req.body;
+
+  try {
+    const { error, value } = acceptFPCodeSchema.validate({
+      email,
+      providedCode,
+      newPassword,
+    });
+
+    if (error) {
+      return res
+        .status(401)
+        .json({ success: false, message: error.details[0].message });
+    }
+
+    const codeValue = providedCode.toString();
+    const existingUser = await User.findOne({ email }).select(
+      '+forgotPasswordCode +forgotPasswordCodeValidation'
+    );
+
+    if (!existingUser) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'User does not exist!' });
+    }
+
+    if (
+      !existingUser.forgotPasswordCode ||
+      !existingUser.forgotPasswordCodeValidation
+    ) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Oops something is broken!' });
+    }
+
+    //expires after 5 min
+    if (
+      Date.now() - existingUser.forgotPasswordCodeValidation >
+      5 * 60 * 1000
+    ) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'code has been expired!' });
+    }
+
+    const hashedCodeValue = hmacProcess(
+      codeValue,
+      process.env.HMAC_VERIFICATION_CODE_SECRET
+    );
+
+    if (hashedCodeValue === existingUser.forgotPasswordCode) {
+      const hashedPassword = await doHash(newPassword, 12);
+      existingUser.password = hashedPassword;
+      existingUser.forgotPasswordCode = undefined;
+      existingUser.forgotPasswordCodeValidation = undefined;
+      await existingUser.save();
+      return res
+        .status(200)
+        .json({ success: true, message: 'Your password has been updated!' });
     }
     return res
       .status(400)
--- routes/main.js
+++ routes/main.js
@@ -2,18 +2,33 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home');
 const authController = require('../controllers/auth');
+const { identifier } = require('../middlewares/identification');
 
 router.get('/', homeController.getIndex);
 router.get('/signup', authController.getSignup);
 router.post('/signup', authController.postSignup);
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
-router.get('/logout', authController.logout);
+router.get('/logout', identifier, authController.logout);
 
-router.patch('/send-verification-code', authController.sendVerificationCode);
+router.patch(
+  '/send-verification-code',
+  identifier,
+  authController.sendVerificationCode
+);
 router.patch(
   '/verify-verification-code',
+  identifier,
   authController.verifyVerificationCode
+);
+router.patch('/change-password', identifier, authController.changePassword);
+router.patch(
+  '/send-forgot-password-code',
+  authController.sendForgotPasswordCode
+);
+router.patch(
+  '/verify-forgot-password-code',
+  authController.verifyForgotPasswordCode
 );
 
 module.exports = router;
